3c1ef51cbaacd0ebbdf544c50c1275bd248aaca0
胡若松 <1183269058@qq.com>
2025-12-06 23:27:06 +0800

    fix: correct bitwidth in MuxLookup default value for Sram_Nx128

--- hdl/chisel/src/coralnpu/SramNx128.scala
+++ hdl/chisel/src/coralnpu/SramNx128.scala
@@ -71,6 +71,6 @@ class Sram_Nx128(tcmEntries: Int) extends Module {
 
   // Mux read output
   val selectedSramRead = RegNext(selectedSram, 0.U(sramSelectBits.W))
-  io.rdata := MuxLookup(selectedSramRead, 0.U(sramAddrBits.W))(
+  io.rdata := MuxLookup(selectedSramRead, 0.U(128.W))(
       (0 until nSramModules).map(i => i.U -> sramModules(i).io.rdata))
 }
