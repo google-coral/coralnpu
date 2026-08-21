ff9371069f5f444e269762e06b3ea73bffe9ad45
Derek Chow <derekjchow@google.com>
2026-08-21 14:34:03 -0700

    Fix lint issues in RvvFrontEnd.
    Cast widths before comparisons and assignments.
    
    Change-Id: I4ad6356af0c0701ac0f7415d373c34a207eff7f6

--- hdl/verilog/rvv/design/RvvFrontEnd.sv
+++ hdl/verilog/rvv/design/RvvFrontEnd.sv
@@ -66,6 +66,7 @@ module RvvFrontEnd#(parameter N = 4,
 );
   localparam COUNTBITS = $clog2(N + 1);
   typedef logic [COUNTBITS-1:0] count_t;
+  typedef logic [CAPACITYBITS-1:0] capacity_t;
 
   // vtype architectural state
   logic vill;
@@ -96,21 +97,21 @@ module RvvFrontEnd#(parameter N = 4,
   assign config_state_valid = config_state_reduction;
   assign config_state = config_state_q;
 
-  logic [CAPACITYBITS-1:0] queue_capacity;
+  capacity_t queue_capacity;
   assign queue_capacity_o = queue_capacity;
   always_comb begin
-    queue_capacity = queue_capacity_i - valid_inst_count_q;
+    queue_capacity = queue_capacity_i - capacity_t'(valid_inst_count_q);
   end
 
   logic inst_accepted [N-1:0];
   count_t valid_inst_count_d;
   always_comb begin
     for (int i = 0; i < N; i++) begin
-      inst_accepted[i] = (valid_in_psum[i] < queue_capacity) && inst_valid_i[i];
+      inst_accepted[i] = (capacity_t'(valid_in_psum[i]) < queue_capacity) && inst_valid_i[i];
       inst_ready_o[i] = inst_accepted[i];
     end
-    valid_inst_count_d = (valid_in_psum[N] < queue_capacity) ?
-        valid_in_psum[N] : queue_capacity;
+    valid_inst_count_d = (capacity_t'(valid_in_psum[N]) < queue_capacity) ?
+        valid_in_psum[N] : count_t'(queue_capacity);
   end
 
   always_ff @(posedge clk or negedge rstn) begin
@@ -320,10 +321,10 @@ module RvvFrontEnd#(parameter N = 4,
           inst_config_state[i+1].ma = 0;
         end else if (avl[i] > vlmax[i]) begin
           // One possible valid impl according to 6.3 of RVV spec.
-          inst_config_state[i+1].vl = vlmax[i];
+          inst_config_state[i+1].vl = vlmax[i][`VL_WIDTH-1:0];
           inst_config_state[i+1].lmul = inst_config_state[i+1].lmul_orig;
         end else begin
-          inst_config_state[i+1].vl = avl[i];
+          inst_config_state[i+1].vl = avl[i][`VL_WIDTH-1:0];
           inst_config_state[i+1].lmul = inst_config_state[i+1].lmul_orig;
         end
 
@@ -483,7 +484,7 @@ module RvvFrontEnd#(parameter N = 4,
 `ifdef ZVT_ON
           mset_writes_rd[i] ? mset_rd_data[i] :
 `endif
-          {{(`XLEN-`VL_WIDTH){1'b0}}, inst_config_state[i+1].vl};
+          {{(`XLEN-(`VL_WIDTH)){1'b0}}, inst_config_state[i+1].vl};
     end
   end
 
